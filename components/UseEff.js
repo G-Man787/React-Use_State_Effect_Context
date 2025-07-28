42c492c629f4c6581007e84a5976d5ed469e42cc
Getachew Akalu <getachewakalu@Getachews-iMac.local>
2025-07-27 23:40:58 -0700

    UseEffect hooks updated

--- components/UseEff.js
+++ components/UseEff.js
@@ -2,25 +2,41 @@
  import React,{useState,useEffect} from "react"
  function useEff(){
 
-    const[data,setData]=useState(null);
-    const[cityInput,setCityInput]=useState('')
+    const[data,setData]=useState([]);
+    const[city,setCityInput]=useState('')
     useEffect(()=>{
-    fetch('https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}')
+    fetch(`http://localhost:4000/weather/${city}`)
     .then((user)=>user.json())
     .then((data)=>setData(data));
-    },[cityInput])
+    },[])
+    const filterHandler=data.filter((value)=>{
+      return value.city===city
+    })
     return (
         <div>
             <h1>My use effect hook practice</h1>
          <input
          type="text"
          placeholder="Enter city...."
-         value={cityInput}
+         value={city}
          onChange={(e)=>setCityInput(e.target.value)}
          style={{width:'30%'}}
          />
-         <h3>{data.location.name}</h3>
-         <p>{data.current.temp_c}°C</p>
+         {filterHandler.map((item)=>(
+           <ul key={item.city}>
+           <li>{item.city}</li>
+            <p>{item.temp_C}</p>
+            <p>{item.temp_F}</p>
+            <p>{item.condition}</p>
+            <p>{item.humidity}</p>
+            <p>{item.sunrise}</p>
+            <p>{item.sunset}</p>
+            <p>{item.air_quality}</p>
+           
+         </ul>
+         )
+        )}
+          
         </div>
     )
  }
